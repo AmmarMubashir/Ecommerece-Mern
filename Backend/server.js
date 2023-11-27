f4aeba291bfe6648985791f56fe2ba8048ec1e94
Ammar <muhammadammarmubashir@gmail.com>
2023-11-28 01:28:03 +0500

    First commit

---
+++ Backend/server.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+// const DB = process.env.DATABASE;
+// 8gZ61keZXKHEDKQw
+const DB =
+  "mongodb+srv://muhammadammarmubashir:8gZ61keZXKHEDKQw@cluster0.wlzikoh.mongodb.net/test?retryWrites=true&w=majority";
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log("DB connection successful!");
+  })
+  .catch((err) => {
+    console.log("DB CONNECTION ERROR!");
+    console.log(err);
+  });
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log("App running on port 8000...");
+});
